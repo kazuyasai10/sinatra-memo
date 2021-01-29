6abfa15dd8dd77b95f42d66909b2a4e6f02f2c1c
kazuyasai10 <kazuya.saito@idealfaro.com>
2021-01-29 16:55:49 +0900

    ポストグレスに接続した

--- model.rb
+++ model.rb
@@ -2,59 +2,53 @@
 
 require 'json'
 require 'securerandom'
+require 'pg'
 
-# 読み込んで
-def load_json(json_file_path)
-  File.open(json_file_path) do |f|
-    hash = JSON.parse(f.read)
+class Memo
+  JSON_FILE_PATH = 'data.json'
+  CONNECTION = PG::Connection.new(dbname: 'memo')
+
+  attr_reader :id, :title, :content
+
+  def initialize(id: '000', title: 'notitle', content: 'no-content')
+    @id = id
+    @title = title
+    @content = content
   end
-end
 
-def save_json(json_file_path, json_data)
-  File.open(json_file_path, 'w') do |io|
-    JSON.dump(json_data, io)
-  end
-end
-
-def make_memo_hash(title, content)
-  id = SecureRandom.uuid
-  { 'id' => id, 'title' => title, 'content' => content }
-end
-
-def get_memos(json_file_path)
-  load_json(json_file_path)['memos']
-end
-
-def get_memo_from_id(json_file_path, id)
-  memos = []
-  json_data = load_json(json_file_path)
-  json_data['memos'].each do |memo|
-    memos << memo if memo['id'] == id
-  end
-  memos[0]
-end
-
-def update_memo_from_id(json_file_path, id, title, content)
-  json_data = load_json(json_file_path)
-  json_data['memos'].each do |memo|
-    if memo['id'] == id
-      memo['title'] = title
-      memo['content'] = content
+  def self.find(id)
+    memos = []
+    memo_tables = CONNECTION.exec("SELECT * FROM memo_table WHERE id= '#{id}';")
+    memo_tables.each do |memo|
+      memos << memo
     end
+    memos[0]
+    Memo.new(id: memos[0]['id'], title: memos[0]['title'], content: memos[0]['content'])
   end
-  save_json(json_file_path, json_data)
-end
 
-def delete_memo_from_id(json_file_path, id)
-  json_data = load_json(json_file_path)
-  json_data['memos'].delete_if do |memo_hash|
-    memo_hash['id'] == id
+  def self.all
+    memos = []
+    memo_tables = CONNECTION.exec('SELECT * FROM memo_table;')
+    memo_tables.each do |memo|
+      memos << Memo.new(id: memo['id'], title: memo['title'], content: memo['content'])
+    end
+    memos
   end
-  save_json(json_file_path, json_data)
-end
 
-def insert_data_to_json(json_file_path, title, content)
-  json_data = load_json(json_file_path)
-  json_data['memos'] << make_memo_hash(title, content)
-  save_json(json_file_path, json_data)
+  def self.create(title: '', content: '')
+    id = SecureRandom.uuid
+    CONNECTION.exec("INSERT INTO memo_table VALUES('#{id}', '#{title}', '#{content}')")
+  end
+
+  def self.update(id, title: '', content: '')
+    query = "UPDATE memo_table 
+    SET title = '#{title}',
+    content = '#{content}'
+    WHERE id = '#{id}'"
+    CONNECTION.exec(query)
+  end
+
+  def self.delete(id)
+    CONNECTION.exec("DELETE FROM memo_table WHERE id = '#{id}'")
+  end
 end
